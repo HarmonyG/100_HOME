8791691b57f4afe933ee65e345258d393d43d3d6
Mani <man2t1@gmail.com>
2014-05-23 18:23:33 +0200

    Test commit

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $titre="";
 include ("include/header.php");
 include ("include/menu.php"); 
-
+// Test
 echo '<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>A faire: mettre un carrousel de photos d animaux à adopter + en dessous: 3 sections "abandon" "modalités d adoption" et "actualites" renvoyant vers les pages correspondantes
 <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>';
 
