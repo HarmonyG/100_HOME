09d20feb5862c8f84018b995013fc0676e2b02c4
HarmonyG <harmony.golmard@gmail.com>
2014-05-23 20:54:47 +0200

    Changements à réaliser
    Harmony

---
+++ Changements_a_faire.php
@@ -0,0 +1,24 @@
+//////////////////////////////////////////////////
+					Harmony
+//////////////////////////////////////////////////
+
+Pour la liste d'actualités : 
+Mettre des étoiles selon la qualité de l'article
+
+Détails articles:
+Créer un bloc de commentaires avec des likes selon si le commentaire est utile ou non
+Si le membre n'est pas connecté : il ne peut pas "voter"
+"x internautes ont trouvé ce commentaire utile" : l'afficher en dehors de la condition if(isset($_SESSION['id']))
+
+date sur firefox
+
+//////////////////////////////////////////////////
+					Mani
+//////////////////////////////////////////////////
+
+Pagination à réaliser
+http://antoine-herault.developpez.com/tutoriels/php/pagination-automatique-en-php/
+
+Si on fait un copié collé d'une page en administration et que l'on se déconnecte: on ne devrait plus pouvoir accéder à la page
+
+Se souvenir de moi : voir pour la connexion
